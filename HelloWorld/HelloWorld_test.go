ddc0ad42b9b26558ca85bf3351da433c2418f063
ravilock <ravi.me.professional@hotmail.com>
2022-05-03 18:48:39 -0300

    feat: use passed name to say hello or use 'world' as default

--- HelloWorld/HelloWorld_test.go
+++ HelloWorld/HelloWorld_test.go
@@ -4,11 +4,26 @@ import (
 	"testing"
 )
 
-func TestHello(t *testing.T) {
-	got := Hello("Lula")
-	want := "Hello, Lula"
+func assertMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func TestHello(t *testing.T) {
+	t.Run("Saying hello to someone", func(t *testing.T) {
+		got := Hello("Lula")
+		want := "Hello, Lula"
+
+		assertMessage(t, got, want)
+	})
+
+	t.Run("Empty string defaults to 'Wold'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertMessage(t, got, want)
+	})
 }
