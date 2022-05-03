c66276adc2368c68f1dcfe2c8c37d47c66e16c15
ravilock <ravi.me.professional@hotmail.com>
2022-05-02 21:11:02 -0300

    feat: say hello to name

--- HelloWorld/HelloWorld_test.go
+++ HelloWorld/HelloWorld_test.go
@@ -5,8 +5,8 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, World"
+	got := Hello("Lula")
+	want := "Hello, Lula"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
