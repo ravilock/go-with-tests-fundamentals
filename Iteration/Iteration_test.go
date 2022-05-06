d5b15209e533b4b10391ef772651900466b2971f
ravilock <ravi.me.professional@hotmail.com>
2022-05-06 17:17:06 -0300

    feat: expect callers to specify how many times the char will be repeated

--- Iteration/Iteration_test.go
+++ Iteration/Iteration_test.go
@@ -13,10 +13,25 @@ func assertMessage(t testing.TB, got, want string) {
 }
 
 func TestRepeat(t *testing.T) {
-	t.Run("Repeat a char 5 times", func (t *testing.T) {
-		repeated := Repeat("a")
-		expected := "aaaaa"
-		
+	t.Run("Repeat a char N times", func(t *testing.T) {
+		n := 15
+		repeated := Repeat("a", n)
+		expected := "aaaaaaaaaaaaaaa"
+
 		assertMessage(t, repeated, expected)
 	})
+
+	t.Run("If the repeatCount is less than 1, it should return an empty string", func(t *testing.T) {
+		n := 0
+		repeated := Repeat("a", n)
+		expected := ""
+
+		assertMessage(t, repeated, expected)
+	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Repeat("a", 15)
+	}
 }
