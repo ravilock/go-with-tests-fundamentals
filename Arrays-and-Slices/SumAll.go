61f3bb60f7059f7fb6ba0f67b0f7ad40480b0e0a
ravilock <ravi.me.professional@hotmail.com>
2022-05-06 18:02:04 -0300

    ref: make slice of expected length for SumAll function

--- Arrays-and-Slices/SumAll.go
+++ Arrays-and-Slices/SumAll.go
@@ -1,8 +1,11 @@
 package Sum
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	for _, numbers := range numbersToSum {
-		sums = append(sums, Sum(numbers))
+func SumAll(numbersToSum ...[]int) []int {
+	numberOfSlices := len(numbersToSum)
+	sums := make([]int, numberOfSlices)
+
+	for index, numbers := range numbersToSum {
+		sums[index] = Sum(numbers)
 	}
 
 	return sums
