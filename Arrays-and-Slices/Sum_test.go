4ad8e1aae931ffb16719c0bd135cdbdba5f3fbc1
ravilock <ravi.me.professional@hotmail.com>
2022-05-06 17:48:24 -0300

    ref: use simple equality operator on test

--- Arrays-and-Slices/Sum_test.go
+++ Arrays-and-Slices/Sum_test.go
@@ -4,14 +4,6 @@ import (
 	"testing"
 )
 
-func assertMessage(t testing.TB, got, want int) {
-	t.Helper()
-
-	if got != want {
-		t.Errorf("expected '%d' got'%d'", want, got)
-	}
-}
-
 func TestSum(t *testing.T) {
 	t.Run("Sum collection of any length", func(t *testing.T) {
 		given := []int{1, 2, 3}
@@ -19,6 +11,8 @@ func TestSum(t *testing.T) {
 		got := Sum(given)
 		want := 6
 
-		assertMessage(t, got, want)
+		if got != want {
+			t.Errorf("expected '%d' got'%d'", want, got)
+		}
 	})
 }
