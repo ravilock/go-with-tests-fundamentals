e5b21ff91a14ba7fb9a0679e53f92da18eb736f0
ravilock <ravi.me.professional@hotmail.com>
2022-05-07 11:58:25 -0300

    fix: check for empty slice before making tail sum

--- Arrays-and-Slices/SumAllTails.go
+++ Arrays-and-Slices/SumAllTails.go
@@ -5,6 +5,10 @@ func SumAllTails(numbersToSum ...[]int) []int {
 	tailsSum := make([]int, numberOfSlices)
 
 	for index, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			tailsSum[index] = 0
+			continue
+		}
 		tail := numbers[1:]
 		tailsSum[index] = Sum(tail)
 	}
